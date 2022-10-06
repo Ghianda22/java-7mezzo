fd85721219f553432f87a4a82800a6dd5bf3ae8b
Valentina <valelore98@gmail.com>
2022-10-06 15:52:19 +0200

    Created a shuffled deck

--- src/com/setteemezzo/Main.java
+++ src/com/setteemezzo/Main.java
@@ -1,5 +1,33 @@
 package com.setteemezzo;
 
-public class Main {
+import java.util.Scanner;
+import java.util.Random;
 
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		Random r = new Random();
+		int[] deck = new int[40];
+		boolean found = true;
+		
+		for(int i = 0; i<40; i++) {
+			do {
+				found = true;
+				int randomNum = r.nextInt(40)+1;
+				for(int j = 0; j < i; j++) {
+					if(deck[j] == randomNum) {
+						found = false;
+					}
+				}
+				if(found) deck[i] = randomNum;
+			}while(!found);
+			
+			System.out.println(deck[i]);
+		}
+		
+		
+		
+		
+		
+	}
 }
